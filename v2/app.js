8845c86da60247f347188e313356e9220f129733
Ezequiel Farioli <efarioli@gmail.com>
2017-03-20 11:10:00 +0000

    set the show route

--- v2/app.js
+++ v2/app.js
@@ -11,7 +11,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -21,23 +22,26 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - Show all Campgrounds
 app.get("/campgrounds", function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     })
     
 });
 
+//CREATE - Add new campground to DB
 app.post("/campgrounds", function(req, res) {
    //get data from form and add to capgrounds array
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name: name, image: image};
+   var desc = req.body.description;
+   var newCampground = {name: name, image: image, description: desc};
    // Create a new campground and save to Db
    Campground.create(newCampground, function(err, newlyCreated) {
        if(err){
@@ -50,8 +54,23 @@ app.post("/campgrounds", function(req, res) {
    
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
    res.render("new");
+});
+
+//SHOW - Shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        };
+    });
+    
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
