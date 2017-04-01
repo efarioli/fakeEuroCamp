4825ddc7e645db279f5703f6d7d3a9e25e50ddcd
zeke farioli <efarioli@gmail.com>
2017-04-01 14:43:46 +0100

    add the add comment functionality connected with correct campground

--- v4/app.js
+++ v4/app.js
@@ -3,13 +3,16 @@ var express    = require("express"),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment    = require("./models/comment"),
     seedDB     = require("./seeds");
 
-seedDB();
+
 
 mongoose.connect("mongodb://localhost/fake_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+seedDB();
 
 
 //SCHEMA SETUP
@@ -87,7 +90,39 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   });
   
 });
+app.post("/campgrounds/:id/comments", function(req, res){
+  //lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+      res.redirect("/campgrounds");
+    }else{
+      //create new comment
+      Comment.create(req.body.comment, function(err,comment){
+        if(err){
+          console.log(err);
+        }else{
+          //connect new comment to campground
+          campground.comments.push(comment);
+          campground.save();
+          //redirect to current campground show page
+          res.redirect("/campgrounds/" + campground._id);
 
+
+        }
+
+      });
+      
+      
+
+    }
+
+  });
+  
+
+});
+
+//===============================================
 app.listen('3000', 'localhost', function() {
     console.log("The fakeCamp Server Has Started!");
 });
